c47c9317152973c4ae00494c88404547b1f0b396
Ilya Guvakov <dev.guwi@gmail.com>
2023-08-24 10:59:34 +0200

    update: almost all tests are done

--- fastapi_walletauth/test_authorization.py
+++ fastapi_walletauth/test_authorization.py
@@ -10,6 +10,7 @@ from eth_account import Account
 from fastapi import HTTPException
 from starlette.testclient import TestClient
 from .router import authorization, create_challenge, refresh_token, solve_challenge
+from .verification import BadSignatureError
 
 putenv("TEST_CHANNEL", "true")
 
@@ -29,17 +30,16 @@ def client():
 
 def test_create_challenge(client):
     chain = SupportedChains.Ethereum.value
-    pubkey = '0x5ce9454909639D2D17A3F753ce7d93fa0b9aB12E'
-    pub = eth_account.account.keys.PublicKey
+    address = '0x5ce9454909639D2D17A3F753ce7d93fa0b9aB12E'
 
-    AuthTokenManager.get_challenge(pubkey, chain)
+    AuthTokenManager.get_challenge(address, chain)
     response = client.post(
         "/authorization/challenge",
-        params={"pubkey": pub, "chain": chain},
+        params={"address": address, "chain": chain},
     )
     assert response.status_code == 200
     data = response.json()
-    assert data["pubkey"] == pubkey
+    assert data["address"] == address
     assert data["chain"] == chain
     assert "challenge" in data
     assert "valid_til" in data
@@ -49,19 +49,19 @@ def test_create_challenge(client):
 @pytest.mark.asyncio
 async def test_solve_challenge(client):
     chain = SupportedChains.Ethereum.value
-    pubkey = Account.get_address()  # There's some kind of strange exception while comparing these adresses
+    address = '0x5ce9454909639D2D17A3F753ce7d93fa0b9aB12E'
 
-    message = await create_challenge(pubkey, chain)
+    message = await create_challenge(address, chain)
 
-    await Account.sign_message(message.challenge)
-    # assert message["signature"]
+    await Account.sign_message(message.challenge["challenge"], '0x8676e9a8c86c8921e922e61e0bb6e9e9689aad4c99082620610b00140e5f21b8')
+    assert message["signature"]
 
     print(message)
 
     response = client.post(
         "/authorization/solve",
         params={
-            "pubkey": pubkey,
+            "address": address,
             "chain": chain,
             "signature": message["signature"],
         },
@@ -69,95 +69,85 @@ async def test_solve_challenge(client):
 
     assert response.status_code == 200
     data = response.json()
-    assert data["pubkey"] == pubkey
+    assert data["address"] == address
     assert data["chain"] == chain
     assert "token" in data
     assert "valid_til" in data
 
-    # wrong_signature = "0x" + "0" * 130
-    # with pytest.raises(BadSignatureError):
-    #     AuthTokenManager.solve_challenge(pubkey, chain, wrong_signature)
-    #     response = client.post("/solve", params={"pubkey": pubkey, "chain": chain, "signature": wrong_signature})
-    #     assert response.status_code == 403
-    #     assert HTTPException(403, "Challenge failed")
+
+def test_solve_challenge_with_forged_signature(client):
+    chain = SupportedChains.Ethereum.value
+    address = '0x5ce9454909639D2D17A3F753ce7d93fa0b9aB12E'
+
+    wrong_signature = "0x" + "0" * 130
+    with pytest.raises(BadSignatureError):
+        AuthTokenManager.solve_challenge(address, chain, wrong_signature)
+        response = client.post("/solve", params={"address": address, "chain": chain, "signature": wrong_signature})
+        assert response.status_code == 403
+        assert HTTPException(403, "Challenge failed")
 
 
-# Isn't working right now
-# @pytest.mark.asyncio
-# async def test_refresh_token(client, ethereum_account):
-#     chain = SupportedChains.Ethereum.value
-#     pubkey = ethereum_account.get_address()
-#
-#     message = asdict(
-#         Message(
-#             "ETH",
-#             ethereum_account.get_address(),
-#             "POST",
-#             "SomeHash",
-#         )
-#     )
-#     await ethereum_account.sign_message(message)
-#     assert message["signature"]
-#
-#     solve_response = await solve_challenge(pubkey, chain, message["signature"])
-#
-#     response = client.post(
-#         "/authorization/refresh",
-#         params={"token": solve_response.token},
-#     )
-#
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert data["pubkey"] == pubkey
-#     assert data["chain"] == chain
-#     assert data["token"] == solve_response.token
-#     assert "valid_til" in data
-#
-#     with pytest.raises(HTTPException):
-#         client.post(
-#             "/authorization/refresh",
-#             params={"token": "0x" + "0" * 130},
-#         )
-#
-#     assert HTTPException(403, "Not authorized")
-#
-#
-# # Isn't working right now
-# @pytest.mark.asyncio
-# async def test_logout(client, ethereum_account):
-#     chain = SupportedChains.Ethereum.value
-#     pubkey = ethereum_account.get_address()
-#
-#     message = asdict(
-#         Message(
-#             "ETH",
-#             ethereum_account.get_address(),
-#             "POST",
-#             "SomeHash",
-#         )
-#     )
-#     await ethereum_account.sign_message(message)
-#     assert message["signature"]
-#
-#     solve_response = await solve_challenge(pubkey, chain, message["signature"])
-#     refresh_response = await refresh_token(solve_response.token)
-#
-#     response = client.post(
-#         "/authorization/logout",
-#         params={"token": refresh_response.token},
-#     )
-#
-#     assert response.status_code == 200
-#
-#
-# # Funktioniert noch nicht
-# def test_challenge_timeout(client):
-#     expired_token = ""  # Muss noch konfigurieret werden
-#
-#     with pytest.raises(HTTPException):
-#         client.post(
-#             "/authorization/refresh",
-#             params={"token": expired_token},
-#         )
-#
-#     assert HTTPException(403, "Token expired")
+@pytest.mark.asyncio
+async def test_refresh_token(client):
+    chain = SupportedChains.Ethereum.value
+    address = '0x5ce9454909639D2D17A3F753ce7d93fa0b9aB12E'
+
+    message = await create_challenge(address, chain)
+
+    await Account.sign_message(message.challenge, '0x8676e9a8c86c8921e922e61e0bb6e9e9689aad4c99082620610b00140e5f21b8')
+    assert message["signature"]
+
+    solve_response = await solve_challenge(address, chain, message["signature"])
+
+    response = client.post(
+        "/authorization/refresh",
+        params={"token": solve_response.token},
+    )
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["address"] == address
+    assert data["chain"] == chain
+    assert data["token"] == solve_response.token
+    assert "valid_til" in data
+
+    with pytest.raises(HTTPException):
+        client.post(
+            "/authorization/refresh",
+            params={"token": "0x" + "0" * 130},
+        )
+
+    assert HTTPException(403, "Not authorized")
+
+
+@pytest.mark.asyncio
+async def test_logout(client):
+    chain = SupportedChains.Ethereum.value
+    address = '0x5ce9454909639D2D17A3F753ce7d93fa0b9aB12E'
+
+    message = await create_challenge(address, chain)
+
+    await Account.sign_message(message.challenge, '0x8676e9a8c86c8921e922e61e0bb6e9e9689aad4c99082620610b00140e5f21b8')
+    assert message["signature"]
+
+    solve_response = await solve_challenge(address, chain, message["signature"])
+    refresh_response = await refresh_token(solve_response.token)
+
+    response = client.post(
+        "/authorization/logout",
+        params={"token": refresh_response.token},
+    )
+
+    assert response.status_code == 200
+
+
+def test_challenge_timeout(client):
+    expired_token = ""
+
+    with pytest.raises(HTTPException):
+        client.post(
+            "/authorization/refresh",
+            params={"token": expired_token},
+        )
+
+    assert HTTPException(403, "Token expired")
