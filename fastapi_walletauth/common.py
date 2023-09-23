98e50d42f4fa1752a8c78bcabb099c8349b0c807
mhh <mike.hukiewitz@robotter.ai>
2023-09-23 18:08:52 +0200

    v1.0.0-beta: upgrade FastAPI and Pydantic to v2.0 & v0.100

--- fastapi_walletauth/common.py
+++ fastapi_walletauth/common.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class SupportedChains(Enum):
@@ -26,10 +26,11 @@ class Settings(BaseSettings):
     TOKEN_TTL: int = 24 * 60 * 60  # 24 hours
     CHALLENGE_TTL: int = 10 * 60  # 10 minutes
 
-    class Config:
-        env_prefix = "FASTAPI_WALLETAUTH_"
-        case_sensitive = False
-        env_file = ".env"
+    model_config = {
+        "env_prefix": "FASTAPI_WALLETAUTH_",
+        "case_sensitive": False,
+        "env_file": ".env",
+    }
 
 
 settings = Settings()
